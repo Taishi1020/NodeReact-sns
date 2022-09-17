a2c22f04c35c5b548d3d94b1e60e3a350f66ff3c
Taishi1020 <tai.yakiman1020@gmail.com>
2022-09-17 12:42:26 +0900

    PORT番号3000→3001に変更 mongDBデータベース接続

--- server.js
+++ server.js
@@ -3,7 +3,22 @@ const app = express();
 const usersRoute = require("./routes/users")
 const authRoute = require("./routes/auth")
 const postRoute = require("./routes/posts")
-const PORT = 3000;
+const PORT = 3001;
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+
+
+//データーベース接続
+mongoose
+    .connect(process.env.MONGURL)
+    .then(() => {
+        console.log("DBと接続中・・・")
+    }).catch((err) => {
+    console.log(err)
+    })
+
+
 //ミドルウェア設定
 app.use("/api/users", usersRoute)
 app.use("/api/auth", authRoute)
@@ -12,9 +27,6 @@ app.use("/api/posts", postRoute)
 app.get("/", (req, res) => {
     res.send("hello express")
 })
-// app.get("/users", (req, res) => {
-//     res.send("hello users")
-// })
 app.listen(PORT, () => console.log("サーバーが起動しました"))
 
 

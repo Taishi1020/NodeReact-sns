575b9e60f5be06775f73c18cb33aa27036170585
Taishi1020 <tai.yakiman1020@gmail.com>
2022-09-21 11:46:03 +0900

    upd: ユーザー情報の削除と特定のユーザー情報を取得するAPI作成

--- routes/users.js
+++ routes/users.js
@@ -6,20 +6,37 @@ const User = require("../models/User")
 router.put("/:id", async (req, res) => {
     if (req.body.userId === req.params.id) {
         try {
-             const user = await User.findByIdAndUpdate(req.params.id, {
+            const user = await User.findByIdAndUpdate(req.params.id, {
                 $set: req.body,
             })
             res.status(200).json("ユーザー情報が更新されました。")
-        }catch (e) {
+        } catch (e) {
             return res.status(400).json(e)
         }
-    }else{
+    } else {
         return res.status(400).json("あなた自身のアカウントのみ更新できます。")
     }
 
 })
 //ユーザー情報を削除するAPI
+router.delete("/:id", async (req, res) => {
+    try {
+        const deleteUser = await User.findByIdAndDelete(req.params.id)
+        console.log(deleteUser)
+        res.status(200).json("ユーザー情報の削除が完了しました。")
+    }catch (e) {
+        return res.status(500).json(e)
+    }
+})
 
-
+//特定のユーザー情報の取得
+router.get("/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        res.status(200).json(user)
+    }catch (e) {
+        return res.status(500).json(e)
+    }
+})
 module.exports = router;
 

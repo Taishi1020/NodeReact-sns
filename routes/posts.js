68d6c883fbf17b21c84bcbb2b60c257dfea06860
Taishi1020 <tai.yakiman1020@gmail.com>
2022-09-22 12:01:49 +0900

    upd: 投稿を更新する用のAPIを修正

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
-const post = require("../models/posts")
+const Post = require("../models/posts")
 
 //投稿
 router.post("/", async (req, res) => {
-    const newPost = new post(req.body)
+    const newPost = new Post(req.body)
     try {
         const savedPost = await newPost.save(); //saveメソッドでmodels/postsのボディに記載したデータ内容がmongooseに登録させる
         res.status(200).json(savedPost)
@@ -15,8 +15,13 @@ router.post("/", async (req, res) => {
 //投稿内容の更新
 router.put("/:id", async (req, res) => {
     try {
-        await post.findByIdAndUpdate(req.params.id,{$set: req.body})
-        res.status(200).json("投稿が更新がされました！")
+        const post = await Post.findById(req.params.id);
+        if (post.userId === req.body.userId) {
+            await post.updateOne({$set: req.body})
+            res.status(200).json("投稿内容を更新することができました！")
+        }else{
+         return res.status(403).json("あなたは他の人の投稿を更新することはできません。")
+        }
     } catch (e) {
         return res.status(500).json(e)
     }

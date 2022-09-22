b833cda78b8f6d0a868b41d188d26fe143af8d9b
Taishi1020 <tai.yakiman1020@gmail.com>
2022-09-22 11:46:25 +0900

    upd: 投稿を更新する用のAPIを作成

--- routes/posts.js
+++ routes/posts.js
@@ -2,12 +2,22 @@ const router = require("express").Router();
 const post = require("../models/posts")
 
 //投稿
-router.post("/", async(req, res) => {
+router.post("/", async (req, res) => {
     const newPost = new post(req.body)
-    try{
+    try {
         const savedPost = await newPost.save(); //saveメソッドでmodels/postsのボディに記載したデータ内容がmongooseに登録させる
         res.status(200).json(savedPost)
-    }catch (e) {
+    } catch (e) {
+        return res.status(500).json(e)
+    }
+})
+
+//投稿内容の更新
+router.put("/:id", async (req, res) => {
+    try {
+        await post.findByIdAndUpdate(req.params.id,{$set: req.body})
+        res.status(200).json("投稿が更新がされました！")
+    } catch (e) {
         return res.status(500).json(e)
     }
 })

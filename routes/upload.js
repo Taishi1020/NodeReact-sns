3746220a4e05060950dbf8fb48d40729732e294a
Taishi1020 <tai.yakiman1020@gmail.com>
2022-10-23 20:39:55 +0900

    upd: 画像アップロードAPIを自作で作成

--- routes/upload.js
+++ routes/upload.js
@@ -1,4 +1,15 @@
 const router =  require("express").Router();
 const multer = require("multer");
 
+const upload = multer()
+
+//画像mアップロード用API
+router.post("/",  upload.single("file"), (req, res) => {
+    try{
+        return res.status(200).json("画像アップロードに成功しました!" )
+    }catch (e) {
+        console.log(e)
+    }
+})
+
 module.exports = router;
